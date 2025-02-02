f6752b7cb4523f665767e13477c2e1c6b2aeb648
LucasJFaust <lucasjfm.eng@gmail.com>
2025-02-02 19:19:20 -0300

    moeda_api lógica

---
+++ tap-moeda/tap_moeda/moeda_api.py
@@ -0,0 +1,21 @@
+from typing import Dict, List
+
+from singer.logger import get_logger
+from tap_moeda.endpoints.last import Last
+from tap_moeda.endpoints.daily import Daily
+from tap_moeda.endpoints.endpoint import Endpoint
+
+
+class Currency(Endpoint):
+    def __init__(self, config: Dict) -> None:
+        self.config = config
+
+    def sync(self, stream: str) -> List:
+        if stream == "last":
+            data = Last(self.config).sync(stream)
+        elif stream == "daily":
+            data = Daily(self.config).sync(stream)
+        else:
+            raise ValueError(f"Unrecognized stream: {stream}")
+
+        return data
